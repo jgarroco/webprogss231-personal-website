b45e287eb02e6a47101cdc549cfe43fcccac3614
apcjgarroco <jgarroco@student.apc.edu.ph>
2024-12-19 19:55:03 +0800

    added another HTML DoM FUNCTION

--- home/js/myscript.js
+++ home/js/myscript.js
@@ -1,3 +1,8 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph Changed.";
 }
+function myFunction2() {
+    const element = document.getElementsByTagName("br");
+ 
+    document.getElementById("demo").innerHTML = 'The text in first <br> tag (index 0) is: ' + element[0].innerHTML;
+}
