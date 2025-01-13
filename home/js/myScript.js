0ffa1c537837c86583294a56747d1058f1f09979
apcjgarroco <jgarroco@student.apc.edu.ph>
2025-01-14 02:15:44 +0800

    final-fix-2

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,71 +1,61 @@
-// Vue Components
-const PictureGallery = {
-    template: `
-      <div class="gallery">
-        <h2>Picture Gallery</h2>
-        <div class="gallery-container">
-          <button @click="prevImage" class="gallery-nav">&lt;</button>
-          <div class="gallery-display">
-            <img :src="images[currentIndex]" alt="Gallery Image">
-          </div>
-          <button @click="nextImage" class="gallery-nav">&gt;</button>
-        </div>
-      </div>
-    `,
+// Picture Gallery Vue App
+const galleryApp = Vue.createApp({
     data() {
-      return {
-        images: [
-          'images/image1.jpg',
-          'images/image2.jpg',
-          'images/image3.jpg'
-        ],
-        currentIndex: 0
-      };
+        return {
+            currentImage: 0,
+            images: ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg']
+        };
     },
     methods: {
-      prevImage() {
-        this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-      },
-      nextImage() {
-        this.currentIndex = (this.currentIndex + 1) % this.images.length;
-      }
+        nextImage() {
+            this.currentImage = (this.currentImage + 1) % this.images.length;
+        },
+        prevImage() {
+            this.currentImage = (this.currentImage - 1 + this.images.length) % this.images.length;
+        }
     }
-  };
-  
-  const DemoGallery = {
-    template: `
-      <div class="gallery demo-container">
-        <div class="gallery-container">
-          <button @click="prevGif" class="demo-nav">&lt;</button>
-          <div class="demo-display">
-            <img :src="gifs[currentGif]" alt="Demo GIF">
-          </div>
-          <button @click="nextGif" class="demo-nav">&gt;</button>
-        </div>
-      </div>
-    `,
+});
+
+galleryApp.mount('#gallery-app');
+
+// Animation Demos Vue App
+const animationApp = Vue.createApp({
     data() {
-      return {
-        gifs: ['gifs/animation1.gif', 'gifs/danimation2.gif'],
-        currentGif: 0
-      };
+        return {
+            currentGif: 0,
+            gifs: ['gifs/gif1.gif', 'gifs/gif2.gif', 'gifs/gif3.gif']
+        };
     },
     methods: {
-      prevGif() {
-        this.currentGif = (this.currentGif - 1 + this.gifs.length) % this.gifs.length;
-      },
-      nextGif() {
-        this.currentGif = (this.currentGif + 1) % this.gifs.length;
-      }
+        nextGif() {
+            this.currentGif = (this.currentGif + 1) % this.gifs.length;
+        },
+        prevGif() {
+            this.currentGif = (this.currentGif - 1 + this.gifs.length) % this.gifs.length;
+        }
     }
-  };
-  
-  const app = Vue.createApp({
-    components: {
-      PictureGallery,
-      DemoGallery
+});
+
+animationApp.mount('#animation-app');
+
+// Guestbook Vue App
+const guestbookApp = Vue.createApp({
+    data() {
+        return {
+            newComment: '',
+            comments: []
+        };
+    },
+    methods: {
+        addComment() {
+            if (this.newComment.trim()) {
+                this.comments.push(this.newComment);
+                this.newComment = '';
+            } else {
+                alert('Please enter a valid comment!');
+            }
+        }
     }
-  });
-  
-  app.mount('#app');
-  
+});
+
+guestbookApp.mount('#guestbook-app');
